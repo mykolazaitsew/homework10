0f4b80ae9394f588c3b5df4559e0dcf27045997a
Zaitsev Mykola <zaitsev.nikolay11@gmail.com>
2024-06-11 18:43:40 +0300

    task2

--- index.js
+++ index.js
@@ -41,9 +41,52 @@ for (const notification of groupedNotifications) {
 }
 
 
-const task2 = () => {
-
-}
+function limit(date) {
+    if (date === 5) {
+      return 23;
+    }
+    console.log(date);
+    return date + 3;
+  }
+  
+  function task2(func) {
+    let cache = new Map();
+    return function (i) {
+      if (cache.has(i)) {
+        return cache.get(i);
+      }
+      let result = func(i);
+      if (![...cache.values()].includes(result)) {
+        if (cache.size < 5) {
+          cache.set(i, result);
+        } else {
+          const [[key0]] = cache;
+          cache.delete(key0);
+          cache.set(i, result);
+        }
+      }
+      return result;
+    };
+  }
+  limit = task2(limit);
+  
+  console.log(limit(5)); 
+  console.log(`Again ${limit(5)}`);
+  
+  console.log(limit(1));
+  console.log(`Again ${limit(10)}`); 
+  
+  console.log(limit(20));
+  console.log(`Again ${limit(20)}`);
+  
+  console.log(limit(30));
+  console.log(`Again ${limit(30)}`);
+  
+  console.log(limit(40)); 
+  console.log(`Again ${limit(40)}`); 
+  
+  console.log(limit(50)); 
+  console.log(`Again ${limit(50)}`); 
 
 
 
